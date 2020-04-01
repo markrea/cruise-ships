9efdc388d502fab51b206cac20793859eb2010bd
Mark Rea <markrea366@gmail.com>
2020-04-01 20:23:14 +0100

    added mocks for test isolation in Ports and Itinerary

--- __tests__/Itinerary.test.js
+++ __tests__/Itinerary.test.js
@@ -1,13 +1,12 @@
 const Itinerary = require('../src/Itinerary.js');
-const Port = require('../src/Port.js');
 
 describe('Itinerary', () => {
     it('can be instantiated', () => {
         expect(new Itinerary()).toBeInstanceOf(Object);
     });
     it('can have ports', () => {
-       const dover = new Port('Dover');
-       const rotterdam = new Port('Rotterdam')
+       const dover = jest.fn('Dover');
+       const rotterdam = jest.fn('Rotterdam')
        
         const itinerary = new Itinerary([dover, rotterdam])
 
--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -12,14 +12,14 @@ describe('Port', () => {
         expect(port.name).toBe('Dover');
     });
     it('can add a ship', () => {
-        const ship = {};
+        const ship = jest.fn();
 
         port.addShip(ship);
         expect(port.ships).toContain(ship);
     });
     it('can remove a ship', () => {
-        const titanic = {};
-        const queenMary = {};
+        const titanic = jest.fn();
+        const queenMary = jest.fn();
 
         port.addShip(titanic);
         port.addShip(queenMary);
